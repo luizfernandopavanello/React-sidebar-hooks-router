0af9017924a0e55bb294f204cc81e2f9d206e488
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-19 19:47:43 -0300

    Finish the sidebar with all the funcionalities working...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+import Team from './pages/Team';
+import Messages from './pages/Messages';
+import Support from './pages/Support';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/reports" component={Reports}/>
+        <Route path="/products" component={Products}/>
+        <Route path="/team" component={Team}/>
+        <Route path="/messages" component={Messages}/>
+        <Route path="/support" component={Support}/>
+      </Switch>
+    </Router>
   );
 }
 
